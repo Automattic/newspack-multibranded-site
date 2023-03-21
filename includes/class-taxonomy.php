9bfb0fcd9cf1ced11b8448e5412a0e9b3ee9a53f
Leo Germani <leogermani@automattic.com>
2023-03-21 08:46:15 -0300

    Set and get primary brands

--- includes/class-taxonomy.php
+++ includes/class-taxonomy.php
@@ -13,10 +13,83 @@ namespace Newspack_Multibranded_Site;
 class Taxonomy {
 
 	/**
+	 * The taxonomy slug.
+	 *
+	 * @var string
+	 */
+	const SLUG = 'brand';
+
+	/**
+	 * The post types to which the taxonomy should be applied.
+	 *
+	 * @var array
+	 */
+	const POST_TYPES = array( 'post', 'page' );
+
+	/**
+	 * The meta key used to flag the primary brand.
+	 *
+	 * @var string
+	 */
+	const PRIMARY_META_KEY = '_primary_brand';
+
+
+	/**
 	 * Runs the initialization.
 	 */
 	public static function init() {
 		add_action( 'init', array( __CLASS__, 'register_taxonomy' ) );
+		add_action( 'add_term_meta', array( __CLASS__, 'reset_primary' ), 10, 2 );
+	}
+
+	/**
+	 * Get the primary brand.
+	 *
+	 * @return ?WP_Term The primary term. If no term is set as primary, the first term will be returned. If no terms exist, null will be returned.
+	 */
+	public static function get_primary() {
+		$params = array(
+			'taxonomy'   => self::SLUG,
+			'hide_empty' => false,
+			'number'     => 1,
+			'meta_query' => array(
+				array(
+					'key'         => self::PRIMARY_META_KEY,
+					'compare_key' => 'EXISTS',
+				),
+			),
+		);
+		$terms  = get_terms( $params );
+		if ( ! empty( $terms ) && $terms[0] instanceof \WP_Term ) {
+			return $terms[0];
+		}
+
+		// if nothing was found, return the first term.
+		$params = array(
+			'taxonomy'   => self::SLUG,
+			'hide_empty' => false,
+			'number'     => 1,
+			'orderby'    => 'term_id',
+			'order'      => 'ASC',
+		);
+		$terms  = get_terms( $params );
+		if ( ! empty( $terms ) && $terms[0] instanceof \WP_Term ) {
+			return $terms[0];
+		}
+	}
+
+	/**
+	 * Sets the primary brand.
+	 *
+	 * @param int|WP_Term $term_or_term_id The term object or the term id.
+	 * @return bool True if the term was set as primary, false otherwise.
+	 */
+	public static function set_primary( $term_or_term_id ) {
+		$term = $term_or_term_id instanceof \WP_Term ? $term_or_term_id : get_term( $term_or_term_id );
+		if ( ! $term instanceof \WP_Term || self::SLUG !== $term->taxonomy ) {
+			return false;
+		}
+		return (bool) add_term_meta( $term->term_id, self::PRIMARY_META_KEY, true );
 	}
 
 	/**
@@ -40,16 +113,38 @@ class Taxonomy {
 		);
 		$params = array(
 			'labels'             => $labels,
-			'hierarchical'       => false,
+			'hierarchical'       => true, // True to have the checkbox UI instead of the tags UI.
 			'publicly_queryable' => false,
-			'show_in_nav_menus'  => false,
-			'show_ui'            => false,
-			'show_admin_column'  => false,
+			'show_in_nav_menus'  => true,
+			'show_in_menu'       => false,
+			'show_ui'            => true,
+			'show_admin_column'  => true,
 			'show_in_rest'       => true,
 			'query_var'          => true,
 			'rewrite'            => array( 'slug' => 'brand' ),
+			'capabilities'       => array(
+				'manage_terms' => 'manage_options',
+				'edit_terms'   => 'manage_options',
+				'delete_terms' => 'manage_options',
+				'assign_terms' => 'edit_posts',
+			),
 		);
-		register_taxonomy( 'brand', array( 'post', 'page' ), $params );
+		register_taxonomy( self::SLUG, self::POST_TYPES, $params );
+	}
+
+	/**
+	 * Hooked into the add_term_meta hook, will clear the primary brand meta key from all terms to make sure we only have one primary brand.
+	 *
+	 * @param int    $term_id The term ID.
+	 * @param string $meta_key Metadata key.
+	 * @return void
+	 */
+	public static function reset_primary( $term_id, $meta_key ) {
+		if ( self::PRIMARY_META_KEY !== $meta_key ) {
+			return;
+		}
+		global $wpdb;
+		$wpdb->query( $wpdb->prepare( "DELETE FROM $wpdb->termmeta WHERE meta_key = %s", self::PRIMARY_META_KEY ) ); //phpcs:ignore
 	}
 
 }
--- tests/unit-tests/test-taxonomy.php
+++ tests/unit-tests/test-taxonomy.php
@@ -5,6 +5,8 @@
  * @package Newspack_Multibranded_Site
  */
 
+use Newspack_Multibranded_Site\Taxonomy;
+
 /**
  * Sample test case.
  */
@@ -14,7 +16,76 @@ class TestTaxonomy extends WP_UnitTestCase {
 	 * A simple test to see if the taxonomy was registered.
 	 */
 	public function test_taxonomy_exists() {
-		// Replace this with some actual testing code.
-		$this->assertTrue( taxonomy_exists( 'brand' ), 'The taxonomy is not registered' );
+		$this->assertTrue( taxonomy_exists( Taxonomy::SLUG ), 'The taxonomy is not registered' );
+	}
+
+	/**
+	 * Test get primary.
+	 */
+	public function test_get_primary() {
+		$term1 = $this->factory->term->create_and_get( array( 'taxonomy' => Taxonomy::SLUG ) );
+		$term2 = $this->factory->term->create_and_get( array( 'taxonomy' => Taxonomy::SLUG ) );
+		$term3 = $this->factory->term->create_and_get( array( 'taxonomy' => Taxonomy::SLUG ) );
+
+		$primary = Taxonomy::get_primary();
+		$this->assertSame( $term1->term_id, $primary->term_id, 'If no primary was set, should return the first' );
+
+		add_term_meta( $term3->term_id, Taxonomy::PRIMARY_META_KEY, true );
+
+		$primary = Taxonomy::get_primary();
+		$this->assertSame( $term3->term_id, $primary->term_id );
+
+		add_term_meta( $term2->term_id, Taxonomy::PRIMARY_META_KEY, true );
+
+		$primary = Taxonomy::get_primary();
+		$this->assertSame( $term2->term_id, $primary->term_id );
+	}
+
+	/**
+	 * Test reset primary on setting a new primary.
+	 */
+	public function test_reset_primary_on_add() {
+		$term1 = $this->factory->term->create_and_get( array( 'taxonomy' => Taxonomy::SLUG ) );
+		$term2 = $this->factory->term->create_and_get( array( 'taxonomy' => Taxonomy::SLUG ) );
+		$term3 = $this->factory->term->create_and_get( array( 'taxonomy' => Taxonomy::SLUG ) );
+
+		global $wpdb;
+		$query = $wpdb->prepare( "SELECT count(term_id) FROM $wpdb->termmeta WHERE meta_key = %s", Taxonomy::PRIMARY_META_KEY );
+
+		// phpcs:disable
+		$this->assertSame( 0, (int) $wpdb->get_var( $query ), 'No primary term should be set' );
+
+		add_term_meta( $term1->term_id, Taxonomy::PRIMARY_META_KEY, true );
+		$this->assertSame( 1, (int) $wpdb->get_var( $query ), 'One primary term should be set' );
+
+		add_term_meta( $term2->term_id, Taxonomy::PRIMARY_META_KEY, true );
+		$this->assertSame( 1, (int) $wpdb->get_var( $query ), 'One primary term should be set' );
+
+		add_term_meta( $term3->term_id, Taxonomy::PRIMARY_META_KEY, true );
+		$this->assertSame( 1, (int) $wpdb->get_var( $query ), 'One primary term should be set' );
+		// phpcs:enable
+	}
+
+	/**
+	 * Test set primary.
+	 */
+	public function test_set_primary() {
+		$term1    = $this->factory->term->create_and_get( array( 'taxonomy' => Taxonomy::SLUG ) );
+		$term2    = $this->factory->term->create_and_get( array( 'taxonomy' => Taxonomy::SLUG ) );
+		$category = $this->factory->term->create_and_get( array( 'taxonomy' => 'category' ) );
+
+		$this->assertTrue( Taxonomy::set_primary( $term1->term_id ), 'Should be able to set a primary term from term id' );
+		$primary = Taxonomy::get_primary();
+		$this->assertSame( $term1->term_id, $primary->term_id );
+
+		$this->assertTrue( Taxonomy::set_primary( $term2 ), 'Should be able to set a primary term from an object' );
+		$primary = Taxonomy::get_primary();
+		$this->assertSame( $term2->term_id, $primary->term_id );
+
+		$this->assertFalse( Taxonomy::set_primary( $category ), 'Should not be able to set a primary term from a category' );
+		$primary = Taxonomy::get_primary();
+		$this->assertSame( $term2->term_id, $primary->term_id );
+
+		$this->assertFalse( Taxonomy::set_primary( array( 'asd' ) ), 'Should return false for invalid input' );
 	}
 }
