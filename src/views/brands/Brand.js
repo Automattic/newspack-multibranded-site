3e44bca77168c12dbf91817b5318fa40fdd11be3
Zakariae Filali <filali.zakariae@gmail.com>
2023-04-06 19:04:54 +0300

    feat(Brand): add/edit a brand

---
+++ src/views/brands/Brand.js
@@ -0,0 +1,180 @@
+import apiFetch from '@wordpress/api-fetch';
+import { addQueryArgs } from '@wordpress/url';
+import { Fragment, useState, useEffect } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { useParams } from 'react-router-dom';
+
+import {
+	Card,
+	Grid,
+	Button,
+	SectionHeader,
+	TextControl,
+	ImageUpload,
+	ColorPicker,
+	SelectControl,
+	ActionCard,
+	withWizardScreen,
+	hooks,
+} from 'newspack-components';
+
+import './style.scss';
+
+const Brand = ( { brands = [], saveBrand } ) => {
+	const [ brand, updateBrand ] = hooks.useObjectState();
+	const [ publicPages, setPublicPages ] = useState( [] );
+
+	const { brandId } = useParams();
+	const selectedBrand = brands.find( ( { id } ) => id === Number( brandId ) );
+
+	useEffect( () => {
+		updateBrand( selectedBrand );
+	}, [ selectedBrand ] );
+
+	const defaultHomepageURLMeta = {
+		label: __( 'Default Page', 'newspack' ),
+		value: 0,
+	};
+
+	const getThemeColor = colorName =>
+		brand.meta?._theme_colors?.find( c => colorName === c.name )?.color;
+
+	const setThemeColor = ( name, color ) => {
+		const themeColors = brand?.meta?._theme_colors ? brand?.meta?._theme_colors : [];
+		const colorIndex = themeColors.findIndex( _color => name === _color.name );
+
+		const updatedThemeColors =
+			colorIndex > -1
+				? themeColors.map( _color => ( name === _color.name ? { ..._color, color } : _color ) )
+				: [ ...themeColors, { name, color } ];
+
+		return updateBrand( {
+			meta: {
+				_theme_colors: updatedThemeColors,
+			},
+		} );
+	};
+
+	const fetchPublicPages = () => {
+		// Limiting to 100 pages, just in case.
+		apiFetch( {
+			path: addQueryArgs( '/wp/v2/pages', { per_page: 100, orderby: 'title', order: 'asc' } ),
+		} ).then( setPublicPages );
+	};
+
+	useEffect( fetchPublicPages, [] );
+
+	// Brand is valid with a name and a logo.
+	const isBrandValid = 0 < brand?.name?.length && 0 < brand?.meta?._logo?.id;
+
+	return (
+		<Fragment>
+			<SectionHeader
+				title={ __( 'Brand', 'newspack' ) }
+				description={ __( 'Set your brand identity', 'newspack' ) }
+			/>
+			<Grid gutter={ 32 }>
+				<Grid columns={ 1 } gutter={ 16 }>
+					<Card noBorder>
+						<TextControl
+							label={ __( 'Name', 'newspack' ) }
+							value={ brand.name || '' }
+							onChange={ updateBrand( 'name' ) }
+						/>
+					</Card>
+				</Grid>
+				<Grid columns={ 1 } gutter={ 16 }>
+					<ImageUpload
+						className="newspack-brand__header__logo"
+						style={ {
+							...( getThemeColor( 'header_background_hex' )
+								? {
+										backgroundColor: getThemeColor( 'header_background_hex' ),
+								  }
+								: {} ),
+						} }
+						label={ __( 'Logo', 'newspack' ) }
+						image={ brand.meta?._logo }
+						onChange={ _logo => updateBrand( { meta: { _logo } } ) }
+					/>
+				</Grid>
+			</Grid>
+
+			<SectionHeader
+				title={ __( 'Colors', 'newspack' ) }
+				description={ __( 'Pick your primary and secondary colors', 'newspack' ) }
+			/>
+			<Grid gutter={ 32 }>
+				<ColorPicker
+					label={ __( 'Primary' ) }
+					color={ getThemeColor( 'primary_color_hex' ) }
+					onChange={ primary_color_hex => setThemeColor( 'primary_color_hex', primary_color_hex ) }
+				/>
+				<ColorPicker
+					label={ __( 'Secondary' ) }
+					color={ getThemeColor( 'secondary_color_hex' ) }
+					onChange={ secondary_color_hex =>
+						setThemeColor( 'secondary_color_hex', secondary_color_hex )
+					}
+				/>
+			</Grid>
+
+			<SectionHeader
+				title={ __( 'Background colors', 'newspack' ) }
+				description={ __( 'Pick your header and footer backgrounds', 'newspack' ) }
+			/>
+			<Grid gutter={ 32 }>
+				<ColorPicker
+					label={ __( 'Header Background' ) }
+					color={ getThemeColor( 'header_background_hex' ) }
+					onChange={ header_background_hex =>
+						setThemeColor( 'header_background_hex', header_background_hex )
+					}
+				/>
+				<ColorPicker
+					label={ __( 'Footer Background' ) }
+					color={ getThemeColor( 'footer_background_hex' ) }
+					onChange={ footer_background_hex =>
+						setThemeColor( 'footer_background_hex', footer_background_hex )
+					}
+				/>
+			</Grid>
+
+			<SectionHeader title={ __( 'Settings', 'newspack' ) } />
+			<Card noBorder>
+				<SelectControl
+					label={ __( 'Homepage URL', 'newspack' ) }
+					value={ brand.meta?._show_page_on_front || '' }
+					options={ [
+						defaultHomepageURLMeta,
+						...publicPages.map( page => ( {
+							label: page.title.rendered,
+							value: Number( page.id ),
+						} ) ),
+					] }
+					onChange={ _show_page_on_front => updateBrand( { meta: { _show_page_on_front } } ) }
+				/>
+			</Card>
+			<ActionCard
+				isMedium
+				title={ __( 'Set as homepage URL', 'newspack' ) }
+				description={ __( 'Whether the brand URL should be at the root of the site.', 'newspack' ) }
+				toggleChecked={ brand?.meta?._custom_url }
+				toggleOnChange={ _custom_url =>
+					updateBrand( { meta: { _custom_url: _custom_url ? 'yes' : 'no' } } )
+				}
+			/>
+
+			<div className="newspack-buttons-card">
+				<Button disabled={ ! isBrandValid } isPrimary onClick={ () => saveBrand( brandId, brand ) }>
+					{ __( 'Save', 'newspack' ) }
+				</Button>
+				<Button isSecondary href="#/">
+					{ __( 'Cancel', 'newspack' ) }
+				</Button>
+			</div>
+		</Fragment>
+	);
+};
+
+export default withWizardScreen( Brand );
---
+++ src/views/brands/index.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { withWizard } from 'newspack-components';
+
+import { addQueryArgs } from '@wordpress/url';
+import { useState, useEffect } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+import BrandsList from './BrandsList';
+import Brand from './Brand';
+
+const Brands = ( { setError, wizardApiFetch } ) => {
+	const [ brands, setBrands ] = useState( [] );
+	const navigate = useNavigate();
+
+	const headerText = __( 'Brands', 'newspack' );
+	const subHeaderText = __( 'Configure brands settings', 'newspack' );
+	const wizardScreenProps = {
+		headerText,
+		subHeaderText,
+	};
+
+	/**
+	 * Fetching brands data.
+	 */
+	const fetchBrands = () => {
+		wizardApiFetch( {
+			path: addQueryArgs( '/wp/v2/brand', { per_page: 100 } ),
+		} )
+			.then( response =>
+				setBrands(
+					response.map( brand => ( {
+						...brand,
+						meta: {
+							...brand.meta,
+							_theme_colors:
+								0 === brand.meta._theme_colors?.length ? null : brand.meta._theme_colors,
+							_menus: 0 === brand.meta._menus?.length ? null : brand.meta._menus,
+						},
+					} ) )
+				)
+			)
+			.catch( error => setError( error ) );
+	};
+
+	const saveBrand = ( brandId, brand ) => {
+		wizardApiFetch( {
+			path: brandId ? `/wp/v2/brand/${ brandId }` : '/wp/v2/brand',
+			method: 'POST',
+			data: {
+				...brand,
+				meta: {
+					...brand.meta,
+					_logo: brand?.meta?._logo ? brand.meta._logo.id : null,
+				},
+			},
+			quiet: true,
+		} )
+			.then( result =>
+				setBrands( brandsList => {
+					// if(brandId)
+					return [ result, ...brandsList ];
+				} )
+			)
+			.then( navigate( '/' ) )
+			.catch( setError );
+	};
+
+	const deleteBrand = brand => {
+		wizardApiFetch( {
+			path: `/wp/v2/brand/${ brand.id }`,
+			method: 'DELETE',
+			quiet: true,
+		} )
+			.then( result => {
+				console.log( 'result', result );
+			} )
+			.catch( e => {
+				setError( e );
+			} );
+	};
+
+	useEffect( fetchBrands, [] );
+
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={
+					<BrandsList { ...wizardScreenProps } brands={ brands } deleteBrand={ deleteBrand } />
+				}
+			/>
+			<Route
+				path="/brands/new"
+				element={
+					<Brand
+						{ ...wizardScreenProps }
+						saveBrand={ saveBrand }
+						setError={ setError }
+						wizardApiFetch={ wizardApiFetch }
+					/>
+				}
+			/>
+			<Route
+				path="/brands/:brandId"
+				element={
+					<Brand
+						{ ...wizardScreenProps }
+						brands={ brands }
+						saveBrand={ saveBrand }
+						setError={ setError }
+						wizardApiFetch={ wizardApiFetch }
+					/>
+				}
+			/>
+		</Routes>
+	);
+};
+
+export default withWizard( Brands );
