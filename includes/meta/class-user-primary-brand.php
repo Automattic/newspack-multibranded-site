6df72b595b70b5093d9d1d2c8f6194de841988e9
Leo Germani <leogermani@automattic.com>
2023-04-06 18:25:30 -0300

    add missing files

---
+++ includes/meta/class-user-primary-brand.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Newspack Multi-branded site taxonomy.
+ *
+ * @package Newspack
+ */
+
+namespace Newspack_Multibranded_Site\Meta;
+
+use Newspack_Multibranded_Site\Meta;
+use Newspack_Multibranded_Site\Taxonomy;
+
+/**
+ * Class to handle the User Primary brand Meta
+ */
+class User_Primary_Brand extends Meta {
+
+	/**
+	 * The meta type
+	 *
+	 * @var string
+	 */
+	public static $type = 'user';
+
+	/**
+	 * Gets the meta key
+	 *
+	 * @return string
+	 */
+	public static function get_key() {
+		return Taxonomy::PRIMARY_META_KEY;
+	}
+
+	/**
+	 * Gets the meta description
+	 *
+	 * @return string
+	 */
+	public static function get_description() {
+		return __( 'The primary brand for a user', 'newspack-multibranded-site' );
+	}
+
+	/**
+	 * Gets the meta schema
+	 *
+	 * @return array
+	 */
+	public static function get_schema() {
+		return array(
+			'type'     => 'integer',
+			'nullable' => true,
+		);
+	}
+
+}
