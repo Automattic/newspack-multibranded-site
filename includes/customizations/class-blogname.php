0a64b9869bccfb6a286db1fc6d4e34c70361fc5c
Leo Germani <leogermani@automattic.com>
2023-04-13 15:47:44 -0300

    feat: filter the blog name

---
+++ includes/customizations/class-blogname.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Newspack Multi-branded site taxonomy.
+ *
+ * @package Newspack
+ */
+
+namespace Newspack_Multibranded_Site\Customizations;
+
+use Newspack_Multibranded_Site\Taxonomy;
+
+/**
+ * Class to handle the Blog Name Customization
+ */
+class Blogname {
+
+	/**
+	 * Initializes
+	 */
+	public static function init() {
+		add_filter( 'option_blogname', [ __CLASS__, 'filter_blogname' ] );
+	}
+
+	/**
+	 * Filters the Blog name
+	 *
+	 * @param string $blogname The blog name.
+	 * @return string
+	 */
+	public static function filter_blogname( $blogname ) {
+		$brand = Taxonomy::get_current();
+		if ( ! $brand ) {
+			return $blogname;
+		}
+		return $brand->name;
+	}
+
+}
