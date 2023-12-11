e67375d52743adc994698ce31e6ab50c785520d5
matticbot <semantic-release-bot@martynus.net>
2023-12-11 17:48:36 +0000

    chore(release): 1.2.0 [skip ci]
    # [1.2.0](https://github.com/Automattic/newspack-multibranded-site/compare/v1.1.0...v1.2.0) (2023-12-11)
    
    ### Bug Fixes
    
    * release process ([5a93b1f](https://github.com/Automattic/newspack-multibranded-site/commit/5a93b1f5ba4449f9ed85a1635f62cd77ce8ee6e5))
    
    ### Features
    
    * use brand in pages set as brand fronts ([#29](https://github.com/Automattic/newspack-multibranded-site/issues/29)) ([7ab39d6](https://github.com/Automattic/newspack-multibranded-site/commit/7ab39d69801ac8639675106f307a75e88ff9511a))

--- newspack-multibranded-site.php
+++ newspack-multibranded-site.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Newspack Multi-branded site
  * Description: A plugin to allow your site to host multiple brands
- * Version: 1.2.0-alpha.1
+ * Version: 1.2.0
  * Author: Automattic
  * Author URI: https://newspack.com/
  * License: GPL3
